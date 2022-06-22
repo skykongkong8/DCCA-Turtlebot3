d55f119e860f7e29dd67caea8266c743d5f70fc8
Sungsik Kong <kssjustin98@gmail.com>
2022-06-22 16:47:09 +0900

    Try 2 for adaptation but failed

--- workspace/dcca_camera/DCCACamera.py
+++ workspace/dcca_camera/DCCACamera.py
@@ -6,21 +6,27 @@ from yolov5.DCCAYoloManager import DCCAYoloManager
 
 
 class DCCACamera(RGBDRealsenseCamera):
+    """
+    DCCACamera contains: 
+    RGB/Depth Streaming, 
+    Object Detection, 
+    Object Depth Clustering,
+    Final Distancing and DCCA DataStructure Formulation
+    """
     def __init__(self):
         super().__init__()
         print("RGBCamera Initialized...")
         self.yoloManager = DCCAYoloManager(self.get_resolution())
         model_setting = self.yoloManager.get_model()
         self.model = model_setting[0]
-        self.stride = model_setting[1]
-        self.device = model_setting[2]
-        self.names = model_setting[3]
+        self.device = model_setting[1]
+        self.names = model_setting[2]
         print("DCCACamera Initialized...")
 
     def yolov5(self, cvView = False):
         flag, rgb_img, depth_img = self.get_frame_stream()
         if flag:
-            detected_image = self.yoloManager.process_model_in_loop(rgb_img, self.model, self.stride, self.device, self.names)
+            detected_image = self.yoloManager.process_model_in_loop(rgb_img, self.model, self.device, self.names)
 
             if cvView:
                 self.yoloManager.view_by_cv2(detected_image, depth_img)
